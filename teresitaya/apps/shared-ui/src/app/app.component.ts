4ac679248eaedd65a7e00f00376748bf6b87d3b4
Maria Rosales <mrosales@itopia.com>
2025-04-03 12:49:48 -0400

    New architecture

--- teresitaya/apps/shared-ui/src/app/app.component.ts
+++ teresitaya/apps/shared-ui/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [RouterModule],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-nx-console';
+  title = 'shared-ui';
 }
