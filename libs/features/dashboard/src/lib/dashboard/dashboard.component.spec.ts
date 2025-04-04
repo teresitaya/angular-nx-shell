0c19b140dc5e1f98f9ab9382f30774cc4f314b9a
Maria Rosales <mrosales@itopia.com>
2025-04-04 17:18:47 -0400

    Fixing unit test

--- libs/features/dashboard/src/lib/dashboard/dashboard.component.spec.ts
+++ libs/features/dashboard/src/lib/dashboard/dashboard.component.spec.ts
@@ -1,13 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
+import { ThemeService } from '@teresitaya/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+    // Mock matchMedia
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),  
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+
     await TestBed.configureTestingModule({
       imports: [DashboardComponent],
+      providers: [ThemeService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
