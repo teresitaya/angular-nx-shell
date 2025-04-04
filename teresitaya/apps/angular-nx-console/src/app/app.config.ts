62c867f99062e4cac9e42f4181fb9cdb21de8c7e
Maria Rosales <mrosales@itopia.com>
2025-04-04 09:51:03 -0400

    Install tailwind and primeng

--- teresitaya/apps/angular-nx-console/src/app/app.config.ts
+++ teresitaya/apps/angular-nx-console/src/app/app.config.ts
@@ -2,10 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+          darkModeSelector: '.my-app-dark'
+        }
+      }
+    })
   ],
 };
--- teresitaya/apps/angular-nx-console/src/app/layout/layout.component.ts
+++ teresitaya/apps/angular-nx-console/src/app/layout/layout.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
+  isDarkMode = false;
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    if (element) {
+      element.classList.toggle('my-app-dark');
+    }
+  }
 }
