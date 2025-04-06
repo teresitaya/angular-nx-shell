699a05c8dfd0e2051e59e44185dc8467dfcb67a3
Maria Rosales <mrosales@itopia.com>
2025-04-06 13:54:59 -0400

    Add favicons, logo and enhance header UI with avatar and tooltips

--- apps/angular-nx-console/src/app/layout/header/header.component.ts
+++ apps/angular-nx-console/src/app/layout/header/header.component.ts
@@ -4,10 +4,24 @@ import { ButtonModule } from 'primeng/button';
 import { Router, RouterLink } from '@angular/router';
 import { ThemeService } from '@teresitaya/core';
 import { Subscription } from 'rxjs';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, ButtonModule, RouterLink],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    RouterLink,
+    AvatarModule,
+    AvatarGroupModule,
+    BadgeModule,
+    OverlayBadgeModule,
+    TooltipModule,
+  ],
   templateUrl: './header.component.html',
   styles: [],
 })
@@ -24,6 +38,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private readonly _router = inject(Router);
 
   private readonly _subscription = new Subscription();
+
   ngOnInit(): void {
     this.isDarkMode = this._themeService.getCurrentTheme() === 'dark';
     this._themeService.theme$.subscribe((theme) => {
@@ -31,11 +46,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
     });
     this._subscription.add(
       this._router.events.subscribe(() => {
-      this.links = this.links.map((link) => ({
-        ...link,
-        active: link.link === this._router.url,
-      }));
-    }));
+        this.links = this.links.map((link) => ({
+          ...link,
+          active: link.link === this._router.url,
+        }));
+      })
+    );
   }
 
   toggleDarkMode() {
